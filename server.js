01c8ae11440716f1017fd3543e9406f2cbd6839f
Abdulkareem Mustapha <aphatheology@gmail.com>
2022-11-11 00:19:53 +0100

    getting started

---
+++ server.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+
+const app = require('./app');
+const connectDB = require('./db/connect');
+
+const port = process.env.PORT || 3000;
+
+
+const start = async () => {
+	try {
+		await connectDB(process.env.MONGO_URL);
+		app.listen(port);
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+start();
